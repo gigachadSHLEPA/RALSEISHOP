b333127cffe85b01573ebd8b00579ba3f32ef5b8
baki <AlexPereGOOD.Off@yandex.ru>
2023-05-09 20:04:12 +0300

    Добавьте файлы проекта.

---
+++ RALSEISHOP/Core/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace RALSEISHOP.Core
+{
+    internal class Core1
+    {
+        public static Frame MyCore { get; set; }
+    }
+}
---
+++ RALSEISHOP/View/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using RALSEISHOP.Core;
+using RALSEISHOP.View.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RALSEISHOP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Core1.MyCore = MainFrame;
+            MainFrame.Navigate(new LOGIN());
+        }
+    }
+}
